ba041d9832070e11a9b0d5a491ac4ac4bcb13ae7
raghav <nautiyal@berkeley.edu>
2024-02-24 17:25:53 -0800

    added audio recording alongside emg data

--- rename_files.py
+++ rename_files.py
@@ -8,13 +8,6 @@ class Rename:
         self.word_list = self.extract_phrases()
         self.utterances = {}
         self.count_utterances()
-        self.data_path = "data"
-
-        # get the filenames that you want to rename - currently gets all the files in the /data directory
-        self.file_names = [f for f in sorted(listdir(self.data_path)) if isfile(join(self.data_path, f))]
-
-        # removes the viz_data.m file -> that shouldn't be renamed
-        self.file_names.remove("viz_data.m")
 
     # extract words from utterance list
     def extract_phrases(self):
@@ -36,11 +29,18 @@ class Rename:
                 self.utterances[i] = 1
 
     # rename each file based on the word and the occurences 
-    def rename_files(self):
+    def rename_files(self, data_path, extension):
+        # get the filenames that you want to rename - currently gets all the files in the specified directory
+        self.file_names = [f for f in sorted(listdir(data_path)) if isfile(join(data_path, f))]
+
+        # removes the viz_data.m file -> that shouldn't be renamed
+        if "viz_data.m" in self.file_names:
+            self.file_names.remove("viz_data.m")
+
         for i in range(len(self.file_names)):
             curr_word = self.word_list[i]
             occurence = self.utterances[curr_word]
-            old_filename = path.join(self.data_path, self.file_names[i])
-            new_filename = path.join(self.data_path, f"{curr_word}_{occurence}.mat")
+            old_filename = path.join(data_path, self.file_names[i])
+            new_filename = path.join(data_path, f"{curr_word}_{occurence}.{extension}")
             rename(old_filename, new_filename)
             self.utterances[curr_word] = occurence + 1
